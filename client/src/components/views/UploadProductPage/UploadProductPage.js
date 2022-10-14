45791e2124a3bbc0c8ca2a304b6a13520fc85cc3
유일 <83910706+minumsa@users.noreply.github.com>
2022-10-14 16:57:31 +0900

    Product.js 파일 생성

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
-import { Typography, Button, Form, Input } from "antd";
-import FileUpload from "../../utils/FileUpload";
+import React, { useState } from 'react';
+import { Typography, Button, Form, Input } from 'antd';
+import FileUpload from '../../utils/FileUpload';
 
 const { TextArea } = Input;
 
 const Continents = [
-  { key: 1, value: "Africa" },
-  { key: 2, value: "Europe" },
-  { key: 3, value: "Asia" },
-  { key: 4, value: "North America" },
-  { key: 5, value: "South America" },
-  { key: 6, value: "Austraila" },
-  { key: 7, value: "Antarctica" },
+  { key: 1, value: 'Africa' },
+  { key: 2, value: 'Europe' },
+  { key: 3, value: 'Asia' },
+  { key: 4, value: 'North America' },
+  { key: 5, value: 'South America' },
+  { key: 6, value: 'Austraila' },
+  { key: 7, value: 'Antarctica' },
 ];
 
 function UploadProductPage() {
-  const [Title, setTitle] = useState("");
-  const [Description, setDescription] = useState("");
+  const [Title, setTitle] = useState('');
+  const [Description, setDescription] = useState('');
   const [Price, setPrice] = useState(0);
   const [Continent, setContinent] = useState(1);
   const [Images, setImages] = useState([]);
 
-  const titleChangeHandler = event => {
+  const titleChangeHandler = (event) => {
     setTitle(event.currentTarget.value);
   };
 
-  const descriptionChangeHandler = event => {
+  const descriptionChangeHandler = (event) => {
     setDescription(event.currentTarget.value);
   };
 
-  const priceChangeHandler = event => {
+  const priceChangeHandler = (event) => {
     setPrice(event.currentTarget.value);
   };
 
-  const continentChangeHandler = event => {
+  const continentChangeHandler = (event) => {
     setContinent(event.currentTarget.value);
   };
 
-  const updateImages = newImages => {
+  const updateImages = (newImages) => {
     setImages(newImages);
   };
 
   return (
-    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
-      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
         <h2>상품 업로드</h2>
       </div>
 
@@ -67,7 +67,7 @@ function UploadProductPage() {
         <br />
         <br />
         <select onChange={continentChangeHandler} value={Continent}>
-          {Continents.map(item => (
+          {Continents.map((item) => (
             <option key={item.key} value={item.key}>
               {item.value}
             </option>
@@ -75,7 +75,7 @@ function UploadProductPage() {
         </select>
         <br />
         <br />
-        <Button>확인</Button>
+        <Button type="submit">확인</Button>
       </Form>
     </div>
   );
