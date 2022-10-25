0bb273590c917859a0e8ac370dac0f5c8d14ca86
유일 <83910706+minumsa@users.noreply.github.com>
2022-10-25 16:49:23 +0900

    카드 만들기(1)

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,54 @@
-import React, { useEffect } from 'react';
-import { FaCode } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { FaCode, FaSortAmountDown } from 'react-icons/fa';
 import axios from 'axios';
+import { Icon, Col, Card, Row } from 'antd';
+import Meta from 'antd/lib/card/Meta';
 
 function LandingPage() {
+  const [Products, setProducts] = useState([]);
+
   useEffect(() => {
     axios.post('/api/product/products').then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        setProducts(response.data.productInfo);
       } else {
         alert('상품들을 가져오는 데 실패했습니다.');
       }
     });
   }, []);
 
-  return <div>Landing Page</div>;
+  const renderCards = Products.map((product, index) => {
+    console.log('product', product);
+
+    return (
+      <Card cover={<img src={`http://localhost:5000/${product.images[0]}`} />}>
+        <Meta title={product.title} description={`$${product.price}`} />
+      </Card>
+    );
+  });
+
+  return (
+    <div style={{ width: '75%', margin: '3rem auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h2>
+          {' '}
+          Let's Shopping! <Icon type="rocket" />
+        </h2>
+      </div>
+
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+
+      {renderCards}
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
