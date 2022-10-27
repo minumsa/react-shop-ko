2ad8148d29aee25aef98ce9a6007601e49ab8205
유일 <83910706+minumsa@users.noreply.github.com>
2022-10-27 15:22:29 +0900

    이미지 슬라이더 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,6 +3,7 @@ import { FaCode, FaSortAmountDown } from 'react-icons/fa';
 import axios from 'axios';
 import { Icon, Col, Card, Row } from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import ImageSlider from '../../utils/ImageSlider';
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
@@ -18,18 +19,9 @@ function LandingPage() {
   }, []);
 
   const renderCards = Products.map((product, index) => {
-    console.log('product', product);
-
     return (
       <Col lg={6} md={8} xs={24} key={index}>
-        <Card
-          cover={
-            <img
-              style={{ width: '100%', maxHeight: '300px' }}
-              src={`http://localhost:5000/${product.images[0]}`}
-            />
-          }
-        >
+        <Card cover={<ImageSlider images={product.images} />}>
           <Meta title={product.title} description={`$${product.price}`} />
         </Card>
       </Col>
