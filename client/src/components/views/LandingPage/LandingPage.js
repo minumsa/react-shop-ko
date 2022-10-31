4be95955b0b0213e421779b19951e4711a003e9f
유일 <83910706+minumsa@users.noreply.github.com>
2022-10-31 18:29:18 +0900

    검색 기능 만들기 #1

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,6 +7,7 @@ import ImageSlider from '../../utils/ImageSlider';
 import Checkbox from './Sections/Checkbox';
 import Radiobox from './Sections/Radiobox';
 import { continents, price } from './Sections/Datas';
+import SearchFeature from './Sections/SearchFeature';
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
@@ -17,6 +18,8 @@ function LandingPage() {
     continents: [],
     price: [],
   });
+
+  const [SearchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     let body = {
@@ -101,6 +104,10 @@ function LandingPage() {
     setFilters(newFilters);
   };
 
+  const updateSearchTerm = (newSearchTerm) => {
+    setSearchTerm(newSearchTerm);
+  };
+
   return (
     <div style={{ width: '75%', margin: '3rem auto' }}>
       <div style={{ textAlign: 'center' }}>
@@ -129,6 +136,15 @@ function LandingPage() {
       </Row>
 
       {/* Search */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          margin: '1rem auto',
+        }}
+      >
+        <SearchFeature refreshFunction={updateSearchTerm} />
+      </div>
 
       {/* Cards */}
 
---
+++ client/src/components/views/LandingPage/Sections/SearchFeature.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Input } from 'antd';
+
+const { Search } = Input;
+
+function SearchFeature(props) {
+  const [SearchTerm, setSearchTerm] = useState('');
+
+  const searchHandler = (event) => {
+    setSearchTerm(event.currentTarget.value);
+    props.refreshFunction(event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <Search
+        placeholder="검색어를 입력하세요"
+        onSearch={searchHandler}
+        style={{ width: 200 }}
+        value={SearchTerm}
+      />
+    </div>
+  );
+}
+
+export default SearchFeature;
