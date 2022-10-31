e54ca6a543089cb029a5f3dfb858fed4c9959224
유일 <83910706+minumsa@users.noreply.github.com>
2022-10-31 16:26:46 +0900

    체크 박스 필터 만들기 #3

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -12,6 +12,10 @@ function LandingPage() {
   const [Skip, setSkip] = useState(0);
   const [Limit, setLimit] = useState(8);
   const [PostSize, setPostSize] = useState();
+  const [Filters, setFilters] = useState({
+    continents: [],
+    price: [],
+  });
 
   useEffect(() => {
     let body = {
@@ -58,7 +62,24 @@ function LandingPage() {
     );
   });
 
-  const handleFilters = () => {};
+  const showFilteredResults = (filters) => {
+    let body = {
+      skip: 0,
+      limit: Limit,
+      filters: filters,
+    };
+
+    getProducts(body);
+    setSkip(0);
+  };
+
+  const handleFilters = (filters, category) => {
+    const newFilters = { ...Filters };
+
+    newFilters[category] = filters;
+
+    showFilteredResults(newFilters);
+  };
 
   return (
     <div style={{ width: '75%', margin: '3rem auto' }}>
@@ -73,7 +94,7 @@ function LandingPage() {
       {/* CheckBox */}
       <Checkbox
         list={continents}
-        handleFilters={(filter) => handleFilters(filters, 'continents')}
+        handleFilters={(filters) => handleFilters(filters, 'continents')}
       />
 
       {/* RadioBox */}
