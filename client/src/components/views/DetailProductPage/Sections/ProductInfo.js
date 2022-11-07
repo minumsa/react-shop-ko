3a5e4c324d80b25c63ff3574ca396a1938090ae1
유일 <83910706+minumsa@users.noreply.github.com>
2022-11-07 12:13:17 +0900

    Product Info 컴포넌트 만들기

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Button, Descriptions } from 'antd';
 
-function ProductInfo() {
-  return <div>ProductInfo</div>;
+function ProductInfo(props) {
+  const clickHandler = () => {};
+
+  return (
+    <div>
+      <Descriptions bordered>
+        <Descriptions.Item label="PRICE">
+          {props.detail.price}
+        </Descriptions.Item>
+        <Descriptions.Item label="SOLD">{props.detail.sold}</Descriptions.Item>
+        <Descriptions.Item label="VIEW">{props.detail.views}</Descriptions.Item>
+        <Descriptions.Item label="ITEM INFO">
+          {props.detail.description}
+        </Descriptions.Item>
+      </Descriptions>
+
+      <br />
+      <br />
+      <br />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button size="large" shape="round" onClick={clickHandler}>
+          ADD TO CART
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductInfo;
