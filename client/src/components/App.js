e1ad6e85226c38227491e93d45746ce63fa4aede
minumsa <icycyi@naver.com>
2022-11-11 18:02:17 +0900

    결제 내역 페이지 만들기

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,16 @@
-import React, { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Auth from '../hoc/auth';
+import React, { Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
 // pages for this product
-import LandingPage from './views/LandingPage/LandingPage.js';
-import LoginPage from './views/LoginPage/LoginPage.js';
-import RegisterPage from './views/RegisterPage/RegisterPage.js';
-import NavBar from './views/NavBar/NavBar';
-import Footer from './views/Footer/Footer';
-import UploadProductPage from './views/UploadProductPage/UploadProductPage.js';
-import DetailProductPage from './views/DetailProductPage/DetailProductPage';
-import CartPage from './views/CartPage/CartPage';
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer";
+import UploadProductPage from "./views/UploadProductPage/UploadProductPage.js";
+import DetailProductPage from "./views/DetailProductPage/DetailProductPage";
+import CartPage from "./views/CartPage/CartPage";
+import HistoryPage from "./views/HistoryPage/HistoryPage";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -19,7 +20,7 @@ function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
@@ -35,6 +36,7 @@ function App() {
             component={Auth(DetailProductPage, null)}
           />
           <Route exact path="/user/cart" component={Auth(CartPage, true)} />
+          <Route exact path="/history" component={Auth(HistoryPage, true)} />
         </Switch>
       </div>
       <Footer />
