19bc4e893365ac63b318a289c692268984ffd6ed
유일 <83910706+minumsa@users.noreply.github.com>
2022-12-07 14:29:04 +0900

    CartPage.js에 각주 추가

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   getCartItems,
   removeCartItem,
   onSuccessBuy,
-} from "../../../_actions/user_actions";
-import UserCardBlock from "./Sections/UserCardBlock";
-import { Empty, Result } from "antd";
-import Paypal from "../../utils/Paypal";
+} from '../../../_actions/user_actions';
+import UserCardBlock from './Sections/UserCardBlock';
+import { Empty, Result } from 'antd';
+import Paypal from '../../utils/Paypal';
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -19,13 +19,14 @@ function CartPage(props) {
     let cartItems = [];
 
     // 리덕스 User state 안의 cart 안에 상품이 들어있는지 확인
+    // &&(Logical AND): The logical AND (&&) (logical conjunction) operator for a set of boolean operands will be true if and only if all the operands are true. Otherwise it will be false.
     if (props.user.userData && props.user.userData.cart) {
       if (props.user.userData.cart.length > 0) {
-        props.user.userData.cart.forEach(item => {
+        props.user.userData.cart.forEach((item) => {
           cartItems.push(item.id);
         });
         dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
-          response => {
+          (response) => {
             calculateTotal(response.payload);
           }
         );
@@ -33,31 +34,32 @@ function CartPage(props) {
     }
   }, [props.user.userData]);
 
-  let calculateTotal = cartDetail => {
+  let calculateTotal = (cartDetail) => {
     let total = 0;
-    cartDetail.map(item => {
+    cartDetail.map((item) => {
       total += parseInt(item.price, 10) * item.quantity;
+      // Addition assignment (+=): The addition assignment (+=) operator adds the value of the right operand to a variable and assigns the result to the variable. The types of the two operands determine the behavior of the addition assignment operator. Addition or concatenation is possible.
     });
 
-    setTotal(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    setTotal(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
     setShowTotal(true);
   };
 
-  let removeFromCart = productId => {
-    dispatch(removeCartItem(productId)).then(response => {
+  let removeFromCart = (productId) => {
+    dispatch(removeCartItem(productId)).then((response) => {
       if (response.payload.productInfo.length <= 0) {
         setShowTotal(false);
       }
     });
   };
 
-  const transactionSuccess = data => {
+  const transactionSuccess = (data) => {
     dispatch(
       onSuccessBuy({
         paymentData: data,
         cartDetail: props.user.cartDetail,
       })
-    ).then(response => {
+    ).then((response) => {
       if (response.payload.success) {
         setShowTotal(false);
         setShowSuccess(true);
@@ -66,7 +68,7 @@ function CartPage(props) {
   };
 
   return (
-    <div style={{ width: "85%", margin: "3rem auto" }}>
+    <div style={{ width: '85%', margin: '3rem auto' }}>
       <h2>장바구니</h2>
       <div>
         <UserCardBlock
@@ -76,10 +78,10 @@ function CartPage(props) {
       </div>
 
       {ShowTotal ? (
-        <div style={{ marginTop: "3rem" }}>
+        <div style={{ marginTop: '3rem' }}>
           <p>
-            <span style={{ fontSize: "15px" }}>전체합계: &nbsp;&nbsp;</span>
-            <span style={{ fontSize: "23px", fontWeight: "bold" }}>
+            <span style={{ fontSize: '15px' }}>전체합계: &nbsp;&nbsp;</span>
+            <span style={{ fontSize: '23px', fontWeight: 'bold' }}>
               {Total}원
             </span>
           </p>
